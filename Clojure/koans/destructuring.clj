2f98fd1cdb3d50e514e69d22f3f1267e1c1660b2
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-05-04 15:24:09 +0200

    Clojure & Scala koans

---
+++ Clojure/koans/destructuring.clj
@@ -0,0 +1,44 @@
+(def test-address
+  {:street-address "123 Test Lane"
+   :city "Testerville"
+   :state "TX"})
+
+(meditations
+  "Destructuring is an arbiter: it breaks up arguments"
+  (= ":bar:foo" ((fn [[a b]] (str b a))
+         [:foo :bar]))
+
+  "Whether in function definitions"
+  (= (str "First comes love, "
+          "then comes marriage, "
+          "then comes Clojure with the baby carriage")
+     ((fn [[a b c]] (format "First comes %s, then comes %s, then comes %s with the baby carriage" a b c))
+      ["love" "marriage" "Clojure"]))
+
+  "Or in let expressions"
+  (= "Rich Hickey aka The Clojurer aka Go Time aka Macro Killah"
+     (let [[first-name last-name & aliases]
+           (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
+       (apply str first-name " " last-name (map (fn [s] (str " aka " s)) aliases))))
+
+  "You can regain the full argument if you like arguing"
+  (= {:original-parts ["Steven" "Hawking"] :named-parts {:first "Steven" :last "Hawking"}}
+     (let [[first-name last-name :as full-name] ["Steven" "Hawking"]]
+       {:original-parts full-name :named-parts {:first first-name :last last-name}}))
+
+  "Break up maps by key"
+  (= "123 Test Lane, Testerville, TX"
+     (let [{street-address :street-address, city :city, state :state} test-address]
+       (format "%s, %s, %s" street-address city state)))
+
+  "Or more succinctly"
+  (= "123 Test Lane, Testerville, TX"
+     (let [{:keys [street-address city state]} test-address]
+       (format "%s, %s, %s" street-address city state)))
+
+  "All together now!"
+  (= "Test Testerson, 123 Test Lane, Testerville, TX"
+     ((fn [[first-name last-name] address]
+       (let [{:keys [street-address city state]} address]
+         (format "%s %s, %s, %s, %s" first-name last-name street-address city state)))
+      ["Test" "Testerson"] test-address)))
---
+++ Scala/koans/AboutPreconditions.scala
@@ -0,0 +1,24 @@
+package org.functionalkoans.forscala
+
+import support.KoanSuite
+
+class AboutPreconditions extends KoanSuite {
+
+  class WithParameterRequirement(val myValue: Int) {
+    require(myValue != 0)
+
+    def this(someValue: String) {
+      this (1)
+    }
+  }
+
+  // Instruction: use Intercept to catch the type of exception thrown by an invalid precondition
+  koan("On precondition violation, intercept expects type of exception thrown") {
+    val myInstance = new WithParameterRequirement("")
+  }
+}
+
+
+
+
+	
